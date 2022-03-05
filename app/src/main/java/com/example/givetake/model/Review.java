b470e2a25842bcca0cef914692e9a7a6ea6f1e2c
Erik Andres Felea Hogas <remoANDcompany@gmail.com>
2022-03-05 21:02:45 +0100

    Review.java new:   - constructor   - setter and getter   - atribute (username)

--- app/src/main/java/com/example/givetake/model/Review.java
+++ app/src/main/java/com/example/givetake/model/Review.java
@@ -1,8 +1,57 @@
 package com.example.givetake.model;
 
 public class Review {
+    private String reviwerName;
     private int score;
     private String comentary;
     private String otherProductName;
     private int extraPrice;
+
+    public Review(String reviwerName, int score, String comentary, String otherProductName, int extraPrice) {
+        this.reviwerName = reviwerName;
+        this.score = score;
+        this.comentary = comentary;
+        this.otherProductName = otherProductName;
+        this.extraPrice = extraPrice;
+    }
+
+    public String getReviwerName() {
+        return reviwerName;
+    }
+
+    public void setReviwerName(String reviwerName) {
+        this.reviwerName = reviwerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComentary() {
+        return comentary;
+    }
+
+    public void setComentary(String comentary) {
+        this.comentary = comentary;
+    }
+
+    public String getOtherProductName() {
+        return otherProductName;
+    }
+
+    public void setOtherProductName(String otherProductName) {
+        this.otherProductName = otherProductName;
+    }
+
+    public int getExtraPrice() {
+        return extraPrice;
+    }
+
+    public void setExtraPrice(int extraPrice) {
+        this.extraPrice = extraPrice;
+    }
 }
